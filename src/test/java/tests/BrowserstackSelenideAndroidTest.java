e542f9cb86d8980fb2c414bebed0422d9420c15a
Anna <zykova.anna.zao@gmail.com>
2021-04-17 21:54:33 +0300

    add tag

--- src/test/java/tests/BrowserstackSelenideAndroidTest.java
+++ src/test/java/tests/BrowserstackSelenideAndroidTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import io.appium.java_client.MobileBy;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
@@ -13,6 +14,7 @@ import static io.qameta.allure.Allure.step;
 public class BrowserstackSelenideAndroidTest extends TestBase {
 
     @Test
+    @Tag("smoke")
     @DisplayName("Search in wikipedia android app")
     void searchWikiTest() {
         step("Type search", () -> {
