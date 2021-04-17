e9796f2abc9a9dc81567ffe3ea2a718ce811f263
Anna <zykova.anna.zao@gmail.com>
2021-04-17 21:49:21 +0300

    tests for android wiki app

---
+++ src/test/java/tests/BrowserstackSelenideAndroidTest.java
@@ -0,0 +1,26 @@
+package tests;
+
+import io.appium.java_client.MobileBy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static io.appium.java_client.MobileBy.AccessibilityId;
+import static io.qameta.allure.Allure.step;
+
+public class BrowserstackSelenideAndroidTest extends TestBase {
+
+    @Test
+    @DisplayName("Search in wikipedia android app")
+    void searchWikiTest() {
+        step("Type search", () -> {
+            $(AccessibilityId("Search Wikipedia")).click();
+            $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).setValue("BrowserStack");
+        });
+        step("Verify content found", () ->
+                $$(MobileBy.id("org.wikipedia.alpha:id/page_list_item_container"))
+                        .shouldHave(sizeGreaterThan(0)));
+    }
+}
